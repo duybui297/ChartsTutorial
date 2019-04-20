48769e8e94d7eed080b3bd468c7926ce45b7e2d8
Duy Bui <48834551+DuyBui297@users.noreply.github.com>
2019-04-20 12:49:32 +0700

    Add files via upload

---
+++ ChartsTutorial/ChartsTutorial/BarChartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BarChartViewController.swift
+//  ChartsTutorial
+//
+//  Created by Duy Bui on 4/20/19.
+//  Copyright © 2019 iOSTemplates. All rights reserved.
+//
+
+import UIKit
+
+class BarChartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ChartsTutorial/ChartsTutorial/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  ChartsTutorial
+//
+//  Created by Duy Bui on 4/20/19.
+//  Copyright © 2019 iOSTemplates. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  var ourStoryboard: UIStoryboard?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    ourStoryboard = UIStoryboard(name: "Main", bundle: nil)
+  }
+  @IBAction func didTapOnPieChartButton(_ sender: Any) {
+    
+    guard let vc = ourStoryboard?.instantiateViewController(withIdentifier: "PieChartViewController") as? PieChartViewController else { return }
+    self.navigationController?.pushViewController(vc, animated: true)
+  }
+  
+  @IBAction func didTapOnLineChartButton(_ sender: Any) {
+    
+    guard let vc = ourStoryboard?.instantiateViewController(withIdentifier: "LineChartViewController") as? LineChartViewController else { return }
+    self.navigationController?.pushViewController(vc, animated: true)
+  }
+  
+  @IBAction func didTapOnBarChartButton(_ sender: Any) {
+    
+    guard let vc = ourStoryboard?.instantiateViewController(withIdentifier: "BarChartViewController") as? BarChartViewController else { return }
+    self.navigationController?.pushViewController(vc, animated: true)
+  }
+}
+
